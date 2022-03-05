9f0a11885939369e9e7609a81189f29ed36463d7
Shattox <stevechen77000@gmail.com>
2022-03-05 14:14:59 +0100

    TASK_1 done

--- src/aircraft_factory.cpp
+++ src/aircraft_factory.cpp
@@ -1,10 +1,15 @@
 #include "aircraft_factory.hpp"
 
-Aircraft* AircraftFactory::create_aircraft(Airport* airport, const AircraftType& type) const
+Aircraft* AircraftFactory::create_aircraft(Airport* airport, const AircraftType& type)
 {
     assert(airport); // make sure the airport is initialized before creating aircraft
 
-    const std::string flight_number = airlines[std::rand() % 8] + std::to_string(1000 + (rand() % 9000));
+    std::string flight_number = airlines[std::rand() % 8] + std::to_string(1000 + (rand() % 9000));
+
+    while (flight_numbers.find(flight_number) != flight_numbers.end()) {
+        flight_number = airlines[std::rand() % 8] + std::to_string(1000 + (rand() % 9000));
+    }
+    flight_numbers.emplace(flight_number);
     const float angle       = (rand() % 1000) * 2 * 3.141592f / 1000.f; // random angle between 0 and 2pi
     const Point3D start     = Point3D { std::sin(angle), std::cos(angle), 0 } * 3 + Point3D { 0, 0, 2 };
     const Point3D direction = (-start).normalize();
--- src/aircraft_factory.hpp
+++ src/aircraft_factory.hpp
@@ -4,11 +4,14 @@
 #include "aircraft_types.hpp"
 #include "airport.hpp"
 
+#include <unordered_set>
+
 class AircraftFactory {
 private:
     const std::string airlines[8] = { "AF", "LH", "EY", "DL", "KL", "BA", "AY", "EY" };
     static constexpr size_t NUM_AIRCRAFT_TYPES = 3;
     AircraftType* aircraft_types[NUM_AIRCRAFT_TYPES] {};
+    std::unordered_set<std::string> flight_numbers;
 
 public:
     AircraftFactory()
@@ -19,6 +22,6 @@ public:
             aircraft_types[2] = new AircraftType { .02f, .1f, .04f, MediaPath { "concorde_af.png" } };
         }
     }
-    Aircraft* create_aircraft(Airport* airport, const AircraftType& type) const;
+    Aircraft* create_aircraft(Airport* airport, const AircraftType& type);
     Aircraft* create_random_aircraft(Airport* airport);
 };
