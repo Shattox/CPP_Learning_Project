596f9f61f86d54264347b84f14f1dfbbb00f92d1
Shattox <stevechen77000@gmail.com>
2022-04-15 09:33:59 +0200

    update TASK_4 1.2

--- src/aircraft.cpp
+++ src/aircraft.cpp
@@ -78,10 +78,10 @@ void Aircraft::operate_landing_gear()
     }
 }
 
-void Aircraft::add_waypoint(const Waypoint& wp, const bool front)
+template <bool front> void Aircraft::add_waypoint(const Waypoint& wp)
 {
     assert(&wp);
-    if (front)
+    if constexpr (front)
     {
         waypoints.push_front(wp);
     }
@@ -99,10 +99,9 @@ bool Aircraft::move()
         {
             return false;
         }
-        auto front = false;
         for (const auto& wp : control.get_instructions(*this))
         {
-            add_waypoint(wp, front);
+            add_waypoint<false>(wp);
         }
     }
 
